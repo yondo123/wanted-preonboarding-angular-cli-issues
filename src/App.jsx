8281cb29a08b395ce885df03e478d7f18403e80a
yondo123 <46988995+yondo123@users.noreply.github.com>
2022-10-30 03:51:52 +0900

    feat: 라우팅 구조 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Layout from './components/Layout';
+import IssueDetail from './pages/IssueDetail';
+import Issuses from './pages/Issues';
+
 function App() {
-  return <div>app</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path="/" element={<Issuses />} />
+            <Route path="/detail" element={<IssueDetail />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
