a29c02f6338c8c579cf09e7fdc14f25822ad321b
SeungJin Choi <46988995+yondo123@users.noreply.github.com>
2022-11-01 17:51:35 +0900

    add: time format 유틸 추가

--- src/utils/format.js
+++ src/utils/format.js
@@ -1,7 +1,9 @@
-export const formatTime = (time) => {
+const formatTime = (time) => {
   const numeric = time.replace(/[^0-9]/g, '');
   return `${numeric.substr(0, 4)}년${numeric.substr(4, 2)}월${numeric.substr(6, 2)}일 ${numeric.substr(
     8,
     2
   )}:${numeric.substr(10, 2)}`;
 };
+
+export default formatTime;
