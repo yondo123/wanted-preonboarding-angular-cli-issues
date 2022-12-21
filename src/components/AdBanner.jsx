ca7c516dd9f25aaf478c7db3f7f274353e8304eb
yondo123 <46988995+yondo123@users.noreply.github.com>
2022-12-21 23:59:34 +0900

    add: 광고배너 컴포넌트

---
+++ src/components/AdBanner.jsx
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+import logo from '../assets/logo-wanted.png';
+
+function AdBanner() {
+  return (
+    <Banner>
+      <a href="https://www.wanted.co.kr/" target="_blank" rel="noopener noreferrer">
+        <img src={logo} alt="wanted" />
+      </a>
+    </Banner>
+  );
+}
+
+const Banner = styled.li`
+  list-style: none;
+  margin-bottom: 12px;
+  border-bottom: solid 1px ${({ theme }) => theme.border};
+  padding: 8px 0;
+
+  img {
+    margin: auto;
+    display: block;
+    height: 100%;
+    max-width: 256px;
+    object-fit: cover;
+  }
+`;
+
+export default AdBanner;
